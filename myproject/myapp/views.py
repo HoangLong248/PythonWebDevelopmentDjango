3d3c0fa157d191426375e5055c519bb8f331594a
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-11-06 23:42:14 +0700

    finished build a counter words section

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {
-        "name" : "Long Tran",
-        "age": 23,
-        "nationality": "Viet Nam"
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    print(amount_of_words)
+    return render(request, 'counter.html', {"amount": amount_of_words})
